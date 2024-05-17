e2cf37b3090257de9c7bf63da91f695dd94291c5
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-05-17 22:40:36 +0100

    update options

--- src/Pages/Options/Options.jsx
+++ src/Pages/Options/Options.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import DropdownButton from "../../Components/DropdownButton/DropdownButton";
 
 function Options() {
+  const Buttons = {
+    title: "Buttons",
+    data: [
+      { label: "Button1", onClick: () => (window.location.href = "/button1") },
+      { label: "Button2", onClick: () => (window.location.href = "/button2") },
+      { label: "Button3", onClick: () => (window.location.href = "/button3") },
+    ],
+  };
+
+  const Modals = {
+    title: "Modals",
+    data: [
+      { label: "Modals1", onClick: () => (window.location.href = "/modals1") },
+      { label: "Modals2", onClick: () => (window.location.href = "/modals2") },
+      { label: "Modals3", onClick: () => (window.location.href = "/modals3") },
+    ],
+  };
+  const DropdownButtons = {
+    title: "DropdownButtons",
+    data: [
+      {
+        label: "DropdownButtons1",
+        onClick: () => (window.location.href = "/dropdownbuttons1"),
+      },
+      {
+        label: "DropdownButtons2",
+        onClick: () => (window.location.href = "/dropdownbuttons2"),
+      },
+      {
+        label: "DropdownButtons3",
+        onClick: () => (window.location.href = "/dropdownbuttons3"),
+      },
+    ],
+  };
   return (
-    <div>Options</div>
-  )
+    <div>
+      <DropdownButton options={Buttons} />
+      <DropdownButton options={Modals} />
+      <DropdownButton options={DropdownButtons} />
+      <DropdownButton options={Buttons} />
+    </div>
+  );
 }
 
-export default Options
+export default Options;
